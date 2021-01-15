d0df211547d164e2a3490dfb5702d1849d3126dc
Ely Hess <Ely.hess@me.com>
2021-01-15 10:55:08 -0700

    Add hospital associations

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :hospitals, only: [] do
+	  resources :surgeries, only: [:index]
+  end
 
   resources :surgeries, only: [:index, :show] do
 	  resources :doctor_surgeries
--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -6,6 +6,8 @@ end
 
 describe 'relationships' do
   it { should have_many :doctors }
+  it { should have_many(:doctor_surgeries).through(:doctors) }
+  it { should have_many(:surgeries).through(:doctor_surgeries) }
 end
 
 describe 'instance methods' do
