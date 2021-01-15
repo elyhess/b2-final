7208dc26d804f7898949ae38d6d3a0b57174c4bb
Ely Hess <Ely.hess@me.com>
2021-01-15 11:42:56 -0700

    Add room_number & surgeries_in_room methods

--- app/models/surgery.rb
+++ app/models/surgery.rb
@@ -1,10 +1,11 @@
 class Surgery < ApplicationRecord
-	has_many :doctor_surgeries
-	has_many :doctors, through: :doctor_surgeries
-	
-	def self.same_day_surgeries(surgery)
-		where(day: surgery.day)
-			.where.not(id: surgery.id)
-	end
+  has_many :doctor_surgeries
+  has_many :doctors, through: :doctor_surgeries
+
+  def self.same_day_surgeries(surgery)
+    where(day: surgery.day)
+      .where.not(id: surgery.id)
+  end
+
 
 end
