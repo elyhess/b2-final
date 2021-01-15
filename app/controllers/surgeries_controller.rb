3fd64d8d32900b64bba347c6caca7b50b776f2bd
Ely Hess <Ely.hess@me.com>
2021-01-15 10:03:44 -0700

    Add user story 2

--- app/controllers/surgeries_controller.rb
+++ app/controllers/surgeries_controller.rb
@@ -1,5 +1,10 @@
 class SurgeriesController < ApplicationController
-	def index
-		@surgeries = Surgery.all
-	end
+  def index
+    @surgeries = Surgery.all
+  end
+
+  def show
+    @surgery = Surgery.find(params[:id])
+    @same_day_surgeries = Surgery.same_day_surgeries(@surgery)
+  end
 end
