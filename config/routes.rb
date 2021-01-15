4711ff8e4d3b8a5a401ea44b9ce5b140f8c7b602
Ely Hess <Ely.hess@me.com>
2021-01-15 11:43:19 -0700

    Add hospital_surgeries resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :hospitals, only: [] do
-	  resources :surgeries, only: [:index]
+	  resources :surgeries, controller: "hospital_surgeries", only: [:index]
   end
 
   resources :surgeries, only: [:index, :show] do
